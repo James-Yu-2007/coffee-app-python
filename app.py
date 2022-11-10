384e575002f7d3aaa8d8d2cc16b13c8dc2f4dd1e
James-Yu-2007 <yujames2007@gmail.com>
2022-11-09 21:33:14 -0800

    second commit

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ PLease choose one of these options:
 4) See which preparation method is best for a bean
 5) Exit
 
-Your selection: 
-"""
+Your selection: """
+
 
 def menu():
     connection = database.connect()
@@ -19,20 +19,45 @@ def menu():
 
     while (user_input := input(MENU_PROMPT)) != "5":
         if user_input == "1":
-            name = input("Enter bean name: ")
-            method = input("Enter how you've prepared it: ")
-            rating = int(input("Enter your rating score (0-100): "))
-
-            database.add_bean(connection, name, method, rating)
+            prompt_add_new_bean(connection)
         elif user_input == "2":
-            beans = database.get_all_beans(connection)
-            for bean in beans:
-                print(bean)
+            prompt_see_all_beans(connection)
         elif user_input == "3":
-            pass
+            prompt_find_bean(connection)
         elif user_input == "4":
-            pass
+            prompt_find_best_method(connection)
         else:
             print("Invalid input, please try again")
+
+
+def prompt_add_new_bean(connection):
+    name = input("Enter bean name: ")
+    method = input("Enter how you've prepared it: ")
+    rating = int(input("Enter your rating score (0-100): "))
+
+    database.add_bean(connection, name, method, rating)
+
+
+def prompt_see_all_beans(connection):
+    beans = database.get_all_beans(connection)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_find_bean(connection):
+    name = input("Enter bean name to find: ")
+    beans = database.get_beans_by_name(connection, name)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_find_best_method(connection):
+    name = input("Enter bean name to find: ")
+    best_method = database.get_best_preparation_for_bean(connection, name)
+
+    print(f"The best preparation method for {name} is: {best_method[2]}.")
+
 
 menu()
